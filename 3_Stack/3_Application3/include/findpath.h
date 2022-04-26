d19190834bb3af891a980c4ee71d8f932438ad9d
yeblee <68188768+33bini@users.noreply.github.com>
2022-04-26 13:43:13 +0900

    Temp maze

---
+++ 3_Stack/3_Application3/include/findpath.h
@@ -0,0 +1,4 @@
+void    findPath(int mazeArray[HEIGHT][WIDTH], MapPosition startPos, MapPosition endPos, LinkedStack *pStack);
+int     pushLSMapPosition(LinkedStack* pStack, MapPosition data);
+void    showPath(LinkedStack *pStack, int mazeArray[HEIGHT][WIDTH]); 
+void    printMaze(int mazeArray[HEIGHT][WIDTH]);
