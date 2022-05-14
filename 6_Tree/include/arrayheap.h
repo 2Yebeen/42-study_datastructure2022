985fd9560d12a6f1aa56943d703f0225b8062fb4
yebeen <yebeen79@naver.com>
2022-05-15 01:01:19 +0900

    Init arrayheap

---
+++ 6_Tree/include/arrayheap.h
@@ -0,0 +1,13 @@
+#ifndef _ARRAY_HEAP_
+#define _ARRAY_HEAP_
+
+// 생성
+insertMaxHeap();
+// 삭제
+deleteMaxHeap
+// 추가
+insertMaxHeapAH(heap, data);
+// 제거
+deleteMaxHeapAH(heap)
+
+#endif _ARRAY_HEAP_
