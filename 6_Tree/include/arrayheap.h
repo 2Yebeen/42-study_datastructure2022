857850a68d134ddc917c500b486b500a7166ada1
yebeen <yebeen79@naver.com>
2022-05-15 01:25:15 +0900

    Init Array Heap

--- 6_Tree/include/arrayheap.h
+++ 6_Tree/include/arrayheap.h
@@ -1,13 +1,30 @@
 #ifndef _ARRAY_HEAP_
 #define _ARRAY_HEAP_
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct ArrayHeapNodeType
+{
+    int key;
+    char data;
+} ArrayHeapNode;
+
+typedef struct ArrayHeapType
+{
+    int maxElementCount;        // 최대 원소 개수
+    int currentElementCount;    // 현재 원소의 개수
+    ArrayHeapNode *pElement;         // 노드 저장을 위한 1차원 배열 포인터
+} ArrayMaxHeap, ArrayMinHeap;
+
 // 생성
-insertMaxHeap();
+ArrayMaxHeap* createArrayMaxHeap(int maxElementCount);
+// 노드 추가
+int insertArrayMaxHeapAH(ArrayMaxHeap* pHeap, ArrayHeapNode element);
+// 노드 제거
+ArrayHeapNode* deleteArrayMaxHeapAH(ArrayMaxHeap* pHeap);
 // 삭제
-deleteMaxHeap
-// 추가
-insertMaxHeapAH(heap, data);
-// 제거
-deleteMaxHeapAH(heap)
+void deleteArrayMaxHeap(ArrayMaxHeap* pHeap);
 
 #endif _ARRAY_HEAP_
---
+++ 6_Tree/src/arrayheap.c
@@ -0,0 +1,17 @@
+#include "../include/arrayheap.h"
+
+ArrayMaxHeap* createArrayMaxHeap(int maxElementCount) {
+
+}
+
+int insertArrayMaxHeapAH(ArrayMaxHeap* pHeap, ArrayHeapNode element) {
+    
+}
+
+ArrayHeapNode* deleteArrayMaxHeapAH(ArrayMaxHeap* pHeap) {
+    
+}
+
+void deleteArrayMaxHeap(ArrayMaxHeap* pHeap) {
+    
+}
