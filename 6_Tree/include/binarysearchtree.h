3b20f0b3842d7a25ffff270fcbcbb1cb2b316a2f
yebeen <yebeen79@naver.com>
2022-05-15 01:01:43 +0900

    Init binary search tree

---
+++ 6_Tree/include/binarysearchtree.h
@@ -0,0 +1,14 @@
+#ifndef _BINARY_SEARCH_TREE_
+#define _BINARY_SEARCH_TREE_
+
+
+
+#endif // _BINARY_SEARCH_TREE_
+
+#ifndef _COMMON_TREE_DEF_
+#define _COMMON_TREE_DEF_
+
+#define TRUE		1
+#define FALSE		0
+
+#endif // _COMMON_TREE_DEF_
