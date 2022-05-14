529f0db5597338fae8778e08cd46cd8e93f20f92
yebeen <yebeen79@naver.com>
2022-05-15 01:48:56 +0900

    Init binary search tree

--- 6_Tree/include/binarysearchtree.h
+++ 6_Tree/include/binarysearchtree.h
@@ -1,7 +1,34 @@
 #ifndef _BINARY_SEARCH_TREE_
 #define _BINARY_SEARCH_TREE_
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+typedef struct BinarySearchTreeNodeType
+{
+    int key;
+	char data;
+	BSTreeNode* pLeftChild;
+	BSTreeNode* pRightChild;
+} BSTreeNode;
+
+typedef struct BinarySearchTreeType
+{
+	BSTreeNode* pRootNode;
+} BSTree;
+
+// 생성
+BSTree* createBinarySearchTree();
+// 추가
+int insertBSTreeNode(BSTree* pBSTree, BSTreeNode element);
+// 검색
+BSTreeNode* searchBSTreeNode(BSTree* pBSTree, int key);
+// 제거
+int deleteBSTreeNode(BSTreeNode* pNode, int key);
+// 삭제
+void deleteBinarySearchTree(BSTree* pBSTree);
+void deleteBinarySEarchTreeNode(BSTreeNode* pNode);
 
 #endif // _BINARY_SEARCH_TREE_
 
---
+++ 6_Tree/src/binarysearchtree.c
@@ -0,0 +1,25 @@
+#include "../include/binarysearchtree.h"
+
+BSTree* createBinarySearchTree() {
+
+}
+
+int insertBSTreeNode(BSTree* pBSTree, BSTreeNode element) {
+
+}
+
+BSTreeNode* searchBSTreeNode(BSTree* pBSTree, int key) {
+
+}
+
+int deleteBSTreeNode(BSTreeNode* pNode, int key) {
+
+}
+
+void deleteBinarySearchTree(BSTree* pBSTree) {
+
+}
+
+void deleteBinarySEarchTreeNode(BSTreeNode* pNode) {
+    
+}
