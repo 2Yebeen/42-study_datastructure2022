f04316876c1da8694e1fe7cd28ab9fc54bc27925
yebeen <yebeen79@naver.com>
2022-05-26 07:27:43 +0900

    Init array graph

---
+++ 7_Graph/src/arraygraph.c
@@ -0,0 +1,54 @@
+# include "../include/arraygraph.h"
+
+// 그래프 생성
+ArrayGraph* createArrayGraph(int maxVertexCount){
+
+}
+
+ArrayGraph* createArrayDirectedGraph(int maxVertexCount){
+
+}
+
+// 그래프 삭제
+void deleteArrayGraph(ArrayGraph* pGraph){
+
+}
+
+// 공백 그래프 여부 판단
+int isEmptyAG(ArrayGraph* pGraph){
+
+}
+
+// 노드 추가
+int addVertexAG(ArrayGraph* pGraph, int vertexID){
+
+}
+
+// 간선 추가
+int addEdgeAG(ArrayGraph* pGraph, int fromVertexID, int toVertexID){
+
+}
+
+int addEdgewithWeightAG(ArrayGraph* pGraph, int fromVertexID, int toVertexID, int weight){
+
+}
+
+// 노드의 유효성 검사
+int checkVertexValid(ArrayGraph* pGraph, int vertexID){
+
+}
+
+// 노드 제거
+int removeVertexAG(ArrayGraph* pGraph, int vertexID){
+
+}
+
+// 간선 제거
+int removeEdgeAG(ArrayGraph* pGraph, int fromVertexID, int toVertexID){
+
+}
+
+// 그래프 정보 출력
+void displayArrayGraph(ArrayGraph* pGraph){
+    
+}
